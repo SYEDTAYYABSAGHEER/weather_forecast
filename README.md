bd092f6101035c96b0021079e7732bd1e62c84dc
SYEDTAYYABSAGHEER <syedtayyabsagheer@gmail.com>
2023-10-20 03:20:34 +0500

    Showing Response

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
 <!-- README.md.erb -->
 
-# My GitHub Profile
+## About Me
 
-The current weather in my city is:
+Ruby on Rails | React JS | Heroku | AWS | Digital Ocean | PHP | Python Scripting
 
-Lahore°C
+## Current Location
 
-Feel free to check out my projects below!
+City: Lahore
+State: Punjab
+Country: Pakistan
+Local Time: 2023-10-20 3:17
+TimeZone: Asia/Karachi
 
+## Weather Condition in Pakistan
 
-[![Update Weather](https://img.shields.io/badge/Update%20Weather-Click%20Here-brightgreen)](https://your-app-url/update_weather)
+Temperature in Celsius: 19.0 °C
+Temperature in Fahrenheit: 19.0 °F
+Condition in Pakistan
+
+{"location"=>{"name"=>"Lahore", "region"=>"Punjab", "country"=>"Pakistan", "lat"=>31.55, "lon"=>74.34, "tz_id"=>"Asia/Karachi", "localtime_epoch"=>1697753869, "localtime"=>"2023-10-20 3:17"}, "current"=>{"last_updated_epoch"=>1697753700, "last_updated"=>"2023-10-20 03:15", "temp_c"=>19.0, "temp_f"=>66.2, "is_day"=>0, "condition"=>{"text"=>"Mist", "icon"=>"//cdn.weatherapi.com/weather/64x64/night/143.png", "code"=>1030}, "wind_mph"=>2.2, "wind_kph"=>3.6, "wind_degree"=>10, "wind_dir"=>"N", "pressure_mb"=>1015.0, "pressure_in"=>29.97, "precip_mm"=>0.0, "precip_in"=>0.0, "humidity"=>78, "cloud"=>0, "feelslike_c"=>19.0, "feelslike_f"=>66.2, "vis_km"=>4.0, "vis_miles"=>2.0, "uv"=>1.0, "gust_mph"=>7.5, "gust_kph"=>12.1}}
--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -12,3 +12,64 @@ class WeatherController < ApplicationController
 
   def index;  end
 end
+
+# My Current Address:
+
+# City: <%= @weather["location"]["name"] %>
+# State: <%= @weather["location"]["region"] %>
+# Country: <%= @weather["location"]["country"] %>
+# Local Time: <%= @weather["location"]["localtime"] %>
+# TimeZone: <%= @weather["location"]["tz_id"] %>
+# Temperature in  Celsius: <%= @weather["current"]["temp_c"] %> °C
+# Temperature in  Fahrenheit: <%= @weather["current"]["temp_c"] %> °F
+# <% if is @weather["condition"]["text"] == 0 %>
+#    Night
+# <%else%>
+#     Day
+# <%end%>
+# Condition in <%= @weather["location"]["country"] %>
+#  <%= @weather["location"]["country"] %>   <%= @weather["location"]["icon"] %>
+
+
+
+# # Feel free to check out my projects below!
+
+
+# # [![Update Weather](https://img.shields.io/badge/Update%20Weather-Click%20Here-brightgreen)](https://your-app-url/update_weather)
+
+# # {"location"=>
+# #   {"name"=>"Lahore",
+# #    "region"=>"Punjab",
+# #    "country"=>"Pakistan",
+# #    "lat"=>31.55,
+# #    "lon"=>74.34,
+# #    "tz_id"=>"Asia/Karachi",
+# #    "localtime_epoch"=>1697752301,
+# #    "localtime"=>"2023-10-20 2:51"},
+# #  "current"=>
+# #   {"last_updated_epoch"=>1697751900,
+# #    "last_updated"=>"2023-10-20 02:45",
+# #    "temp_c"=>20.0,
+# #    "temp_f"=>68.0,
+# #    "is_day"=>0,
+# #    "condition"=>
+# #     {"text"=>"Overcast",
+# #      "icon"=>"//cdn.weatherapi.com/weather/64x64/night/122.png",
+# #      "code"=>1009},
+# #    "wind_mph"=>2.2,
+# #    "wind_kph"=>3.6,
+# #    "wind_degree"=>10,
+# #    "wind_dir"=>"N",
+# #    "pressure_mb"=>1015.0,
+# #    "pressure_in"=>29.97,
+# #    "precip_mm"=>0.0,
+# #    "precip_in"=>0.0,
+# #    "humidity"=>68,
+# #    "cloud"=>0,
+# #    "feelslike_c"=>20.0,
+# #    "feelslike_f"=>68.0,
+# #    "vis_km"=>4.0,
+# #    "vis_miles"=>2.0,
+# #    "uv"=>1.0,
+# #    "gust_mph"=>8.9,
+# #    "gust_kph"=>14.4}}
